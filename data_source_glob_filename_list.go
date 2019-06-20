89d43c45fe67063c28c597ae26c55b1c886302fe
Jake Sanders <i@am.so-aweso.me>
2019-06-20 16:27:33 +0100

    Add basenames to filename list data source

--- data_source_glob_filename_list.go
+++ data_source_glob_filename_list.go
@@ -23,6 +23,11 @@ func dataSourceGlobFilenameList() *schema.Resource {
 				Elem:     &schema.Schema{Type: schema.TypeString},
 				Computed: true,
 			},
+			"basenames": &schema.Schema{
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
 		},
 	}
 }
@@ -35,10 +40,13 @@ func dataSourceGlobFilenameListRead(d *schema.ResourceData, m interface{}) error
 	}
 	sort.Strings(items)
 	var elemhash string
+	var basenames []string
 	for i, s := range items {
 		elemhash += strconv.Itoa(i) + s
+		basenames = append(basenames, filepath.Base(s))
 	}
 	d.SetId(fmt.Sprintf("%x", sha256.Sum256([]byte(elemhash))))
 	d.Set("matches", items)
+	d.Set("basenames", basenames)
 	return nil
 }
